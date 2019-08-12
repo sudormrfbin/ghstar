32c6fb05a1b31d86ab199eaa5994e336bcf04ced
Gokul <gokulps15@gmail.com>
2019-08-12 20:39:52 +0530

    Refactor into functions

--- ghstar.py
+++ ghstar.py
@@ -29,43 +29,86 @@ class InvalidRepoError(Exception):
         super().__init__(msg)
 
 
-example_text = textwrap.dedent(  # ignore common whitespace for all lines
-    """
-    examples:
-      ghstar microsoft/vscode
-      ghstar jlevy/the-art-of-command-line
-    """
-)
-
-parser = argparse.ArgumentParser(
-    prog="ghstar",
-    description="Star GitHub repos from the command line.",
-    epilog=example_text,
-    # specify formatter_class to preserve newlines in epilog
-    formatter_class=argparse.RawDescriptionHelpFormatter,
-)
-parser.add_argument("repo", help="Name of repo to star")
-args = parser.parse_args()
-
-gh_user = os.getenv("GH_UNAME")
-gh_token = os.getenv("GH_TOKEN")
-
-if not (gh_user and gh_token):
-    exit(AuthError())
-
-try:
-    r = requests.put(
-        url="https://api.github.com/user/starred/" + args.repo,
-        auth=(gh_user, gh_token),
-        headers={"Content-Length": "0"},
+def get_argparser():
+    """Returns an `ArgumentParser()` for parsing command line options."""
+    example_text = textwrap.dedent(  # ignore common whitespace for all lines
+        """
+        examples:
+          ghstar microsoft/vscode
+          ghstar jlevy/the-art-of-command-line
+        """
     )
-except requests.exceptions.ConnectionError:
-    exit(ConnectionError("Please check your internet connection."))
 
-if r.status_code == 204:
+    parser = argparse.ArgumentParser(
+        prog="ghstar",
+        description="Star GitHub repos from the command line.",
+        epilog=example_text,
+        # specify formatter_class to preserve newlines in epilog
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    parser.add_argument("repo", help="Name of repo to star")
+
+    return parser
+
+
+def get_credentials():
+    """
+    Get GitHub user credentials from the environment variables
+    `GH_UNAME` and `GH_TOKEN`.
+
+    Returns:
+        tuple: Credentials as a tuple of (username, token).
+
+    Raises:
+        AuthError: If username or token is empty.
+    """
+
+    gh_user = os.getenv("GH_UNAME")
+    gh_token = os.getenv("GH_TOKEN")
+
+    if not (gh_user and gh_token):
+        raise AuthError()
+
+    return (gh_user, gh_token)
+
+
+def star_repo(repo, gh_user, gh_token):
+    """
+    Star a repo on GitHub.
+
+    Raises:
+        AuthError: Invalid user credentials.
+        InvalidRepoError: Invalid repo name.
+        ConnectionError: No internet connection.
+    """
+    try:
+        r = requests.put(
+            url="https://api.github.com/user/starred/" + repo,
+            auth=(gh_user, gh_token),
+            headers={"Content-Length": "0"},
+        )
+    except requests.exceptions.ConnectionError:
+        raise ConnectionError("Please check your internet connection.")
+
+    if r.status_code == 401:
+        raise AuthError()
+    elif r.status_code == 404:
+        raise InvalidRepoError(repo)
+
+
+def main():
+    parser = get_argparser()
+    args = parser.parse_args()
+
+    try:
+        username, token = get_credentials()
+        star_repo(repo=args.repo, gh_user=username, gh_token=token)
+    except (AuthError, InvalidRepoError, ConnectionError) as error:
+        exit(error)
+
     print("Starred " + args.repo)
     exit(0)
-elif r.status_code == 401:
-    exit(AuthError())
-elif r.status_code == 404:
-    exit(InvalidRepoError(args.repo))
+
+
+if __name__ == "__main__":
+    main()
